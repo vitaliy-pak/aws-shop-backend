3bb5fdad2e4d10acc5c991f1f41a16846ea4f475
Vitaliy Pak <vitaliy_pak@epam.com>
2024-10-30 18:40:52 +0500

    update: updated deploy command to deploy all stacks

--- bin/aws-shop-backend.ts
+++ bin/aws-shop-backend.ts
@@ -5,5 +5,5 @@ import { ProductServiceStack } from "../lib/product-service-stack";
 import { ImportServiceStack } from "../lib/import-service-stack";
 
 const app = new cdk.App();
-//new ProductServiceStack(app, 'ProductServiceStack', {});
+new ProductServiceStack(app, 'ProductServiceStack', {});
 new ImportServiceStack(app, 'ImportServiceStack', {});
